71837003ac6ee435fdf0890aeece3f898e690408
Vincent Phillip Rodriguez <54683188+VPRCSUF@users.noreply.github.com>
2019-09-10 22:49:36 -0700

    Update cars.cpp
    Figured out how to loop through if the user inputs invalid numbers.

--- cars.cpp
+++ cars.cpp
@@ -4,9 +4,18 @@
 using namespace std;
 
 int main() {
-string carS[5] = {"> BMW", "> Ford", "> Maserati", "> Lamborghini",
-"> Honda"};
-int typeCar;
+  string carS[5] = {"> BMW", "> Ford", "> Maserati", "> Lamborghini",
+  "> Honda"};
+
+  int typeCar;
+  int modelCar;
+
+  string carB[3] = {" M4", " X6", " i8"};
+  string carF[3] = {" Fiesta", " Mustang", " F-150"};
+  string carM[3] = {" GranTurismo", " Quattroporte", " Levante"};
+  string carL[3] = {" Huracan EVO", " Urus", " Aventador S"};
+  string carH[3] = {" Fit", " CRV", " Civic"};
+
   cout << endl;
   cout << "###########################" << endl;
   cout << "#  RANDOM CAR GENERATOR!  #" << endl;
@@ -14,34 +23,39 @@ int typeCar;
   cout << endl;
   cout << " Pick a number 0-4: " << endl;
   cin >> typeCar;
-  if ( typeCar > 4 ) {
-    cout << "Invalid! Please chose a # between 0-4!" << endl;
-  }
-string carB[3] = {" M4", " X6", " i8"};
-string carF[3] = {" Fiesta", " Mustang", " F-150"};
-string carM[3] = {" GranTurismo", " Quattroporte", " Levante"};
-string carL[3] = {" Huracan EVO", " Urus", " Aventador S"};
-string carH[3] = {" Fit", " CRV", " Civic"};
-int modelCar;
-  cout << endl;
-  cout << " Pick another number 0-2:" << endl;
+
+  //Loops until user inputs a value less than or equal to 4.
+  do {
+    if ( typeCar > 4 ) {
+      cout << "Invalid! Please chose a # between 0-4!" << endl;
+      cin >> typeCar;
+    }
+  }  while (typeCar > 4);
+
+  cout << "\n Pick another number 0-2:" << endl;
   cin >> modelCar;
-  cout << endl;
-  cout << "Congrats, you're the proud owner of a:" << endl;
+
+  //Loops until user inputs a value less than or equal to 2.
+  do {
+    if (modelCar > 2) {
+      cout << "Invalid! Make sure you select a number" << endl;
+      cout << "within the said parameters." << endl;
+      cin >> modelCar;
+    }
+  } while (modelCar > 2);
+
+  cout << "\nCongrats, you're the proud owner of a:" << endl;
   if (typeCar == 0) {
-  cout << carS[typeCar] << carB[modelCar] << endl;
-} else if (typeCar == 1) {
-  cout << carS[typeCar] << carF[modelCar] << endl;
-} else if (typeCar == 2) {
-  cout << carS[typeCar] << carM[modelCar] << endl;
-} else if (typeCar == 3) {
-  cout << carS[typeCar] << carL[modelCar] << endl;
-} else if (typeCar == 4) {
-  cout << carS[typeCar] << carH[modelCar] << endl;
-} else if (modelCar > 4) {
-  cout << "Invalid! Make sure you select a number" << endl;
-  cout << "within the said parameters." << endl;
-}
-// Error loop neeed if incorrect number is entered!
-  return 0;
+    cout << carS[typeCar] << carB[modelCar] << endl;
+  } else if (typeCar == 1) {
+    cout << carS[typeCar] << carF[modelCar] << endl;
+  } else if (typeCar == 2) {
+    cout << carS[typeCar] << carM[modelCar] << endl;
+  } else if (typeCar == 3) {
+    cout << carS[typeCar] << carL[modelCar] << endl;
+  } else if (typeCar == 4) {
+    cout << carS[typeCar] << carH[modelCar] << endl;
+  }
+
+    return 0;
 }
